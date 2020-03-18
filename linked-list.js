aed252d11aef893a7b0d62282552af0deed9d2c4
joshua hahn <tailrin.jh@gmail.com>
2020-03-17 21:30:10 -0600

    added insertion functions to class

---
+++ linked-list.js
@@ -0,0 +1,42 @@
+class _Node {
+    constructor(element) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList{
+    constructor(){
+        this.head = null;
+        this.size = 0;
+    }
+
+    insertFirst(element){
+        const node = new _Node(element);
+        node.next = this.head;
+        this.head = element;
+    }
+
+    insertLast(element){
+        const node = new _Node(element)
+        let current;
+        if(this.head === null){
+            this.head = node;
+        } else {
+            current = this.head;
+
+            while(current.next){
+                current = current.next;
+            }
+
+            current.next = node;
+        }
+
+        this.size++;
+    }
+
+    remove(node){
+        
+    }
+}
+
